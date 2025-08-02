78086f0c4413a66a175aa29eff626025b032a2b8
Promise30 <promiseadeagbo@gmail.com>
2025-08-02 16:33:45 +0100

    Updated project migration

---
+++ Promise.MVC.Movies/Promise.MVC.Movies/Migrations/20250802142401_AddLogModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Promise.MVC.Movies.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLogModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LogEntries",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Message = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StackTrace = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LogEntries", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "1", null, "Admin", "ADMIN" },
+                    { "2", null, "User", "USER" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[,]
+                {
+                    { "1", 0, "694d9c7c-decb-46f9-bae9-94d2f6c3fb3b", "johndoe@gmail.com", true, false, null, "JOHNDOE@GMAIL.COM", "JOHNDOE", "AQAAAAIAAYagAAAAEHL9jga9mbTptjayg2GUXwUjo8BpUbeYKN+xfewDu4O3YJ3wjDJg2xq3QPp24e4Ywg==", "1234567890", true, "c3a81fab-6839-4eea-894e-1fd066b82ca2", false, "JohnDoe" },
+                    { "2", 0, "cd95b0c2-35d1-4e10-a235-3499b5b09a7d", "janedoe@gmail.com", true, false, null, "JANEDOE@GMAIL.COM", "JANEDOE", "AQAAAAIAAYagAAAAEGcLgYduk773EfWooVbzp03KFLjIU1S7XQtleT+uZg8u4oNAyqkF53rKWvCJJb8HdA==", "0987654321", true, "9fff7e89-805c-4afe-bc03-5817f91ad8c2", false, "JaneDoe" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LogEntries");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "1");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "2");
+        }
+    }
+}
